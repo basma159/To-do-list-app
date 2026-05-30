d41014699b1ff6390abee401fede17533adf2f31
Basma <bsmtbdalawl@gmail.com>
2026-05-30 15:02:09 +0300

    test

--- js/script.js
+++ js/script.js
@@ -1,29 +1,4 @@
 
-// let temperature = document.querySelector(".container h1")
-// let time = document.querySelector(".container span")
-
-// async function get() {
-//     const response = await fetch("https://api.open-meteo.com/v1/forecast?latitude=26.55&longitude=31.69&current_weather=true")
-//     const data = await response.json()
-//     console.log(data)
-//     console.log(data.results[0])
-//     if (data.current_weather.temperature > 30) {
-//         document.body.style.background = "orange"
-//     }
-//     // console.log(data.current_weather.temperature)
-//     // let date =new Date(data.current_weather.time)
-//     temperature.innerHTML = `temperature in sohag: ${data.current_weather.temperature} °C `
-//     setInterval(() => {
-//         time.innerHTML = `time in sohag: ${new Date().toLocaleTimeString("en-us", {
-//             timeZone: "Africa/Cairo"
-//         })} `
-//     },1000)
-
-// }
-
-// get()
-// ////////////////////////////////////////////////
-
 let input = document.querySelector("#input")
 let list = document.querySelector(".lists")
 let addBtn = document.querySelector(".addBtn")
@@ -35,9 +10,15 @@ addBtn.addEventListener("click", function () {
     }
     else {
         let tasks = JSON.parse(localStorage.getItem("tasks")) || []
-        tasks.push(input.value)
+        console.log(localStorage.getItem("tasks"))
+        let newTask = {
+            id: Date.now(),
+            text: input.value,
+            completed: false
+        }
+        tasks.push(newTask)
         localStorage.setItem("tasks", JSON.stringify(tasks))
-        createTask(input.value)
+        createTask(newTask)
         input.value = ""
     }
 
@@ -60,6 +41,7 @@ function createTask(taskText, index) {
     div2.appendChild(checkBox)
     div2.appendChild(span)
     div1.appendChild(button)
+    div1.dataset.id = taskText.id
     div1.style.display = "flex"
     div1.style.justifyContent = "center"
     div1.style.alignItems = "center"
@@ -69,28 +51,44 @@ function createTask(taskText, index) {
     div2.style.color = "#007979"
     div2.style.borderRadius = "20px"
     div2.style.padding = "15px"
-    li.textContent = taskText
+    li.textContent = taskText.text
+    checkBox.checked = taskText.completed
+    if (taskText.completed) {
+        li.style.textDecoration = "line-through"
+        span.textContent = "finished"
+    }
+    else {
+        li.style.textDecoration = "none"
+        span.textContent = "not finish"
+    }
 
     button.onclick = function () {
-        let tasks =JSON.parse(localStorage.getItem("tasks"))||[]
-        tasks.splice(index,1)
-        localStorage.setItem("tasks",JSON.stringify(tasks))
+        let tasks = JSON.parse(localStorage.getItem("tasks")) || []
+        tasks = tasks.filter(task => task.id !== taskText.id)
+        localStorage.setItem("tasks", JSON.stringify(tasks))
         div1.remove()
-        
+
     }
     checkBox.onchange = function () {
+        let tasks = JSON.parse(localStorage.getItem("tasks")) || []
+
+        let task = tasks.find(t => t.id === taskText.id)
+        task.completed = checkBox.checked
+
+        localStorage.setItem("tasks", JSON.stringify(tasks))
+
         li.style.textDecoration = checkBox.checked ? "line-through" : "none"
         span.textContent = checkBox.checked ? "finished" : "not finish"
         div2.style.padding = checkBox.checked ? "15px 30px" : "20px"
+
     }
 }
 
 
 function showTasks() {
     let tasks = JSON.parse(localStorage.getItem("tasks")) || []
-
-    tasks.forEach((task, index) => {
-        createTask(task, index)
+    tasks.forEach((task) => {
+        createTask(task)
 
     });
 }
